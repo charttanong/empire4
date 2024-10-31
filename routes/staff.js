5874dc6192125e1ec0825cc8d4fe46d08e087bd9
charttanong <charttanong_zza@hotmail.com>
2024-10-31 09:21:02 +0700

    Fix path for staff model in routes/staff.js

--- routes/staff.js
+++ routes/staff.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const Staff = require('../models/staff');
+const Staff = require('../models/Staff');
 
 const router = express.Router();
 
